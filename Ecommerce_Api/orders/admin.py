8856e82da3a96da36cf7c27ef917c1a6bf98c02c
eeyueltesfaye <eyueltesfaye6@gmail.com>
2024-10-15 00:07:20 +0300

    Registered new models in admin.py for better management

--- Ecommerce_Api/orders/admin.py
+++ Ecommerce_Api/orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order  
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_price', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'user__email')
+    ordering = ('-created_at',)
+
+admin.site.register(Order, OrderAdmin)
